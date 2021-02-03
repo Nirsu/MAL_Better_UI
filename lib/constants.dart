1e3f78143323bf507d27613d6e121e6bae742cb2
Melvyn <melvyn.fontaine@epitech.eu>
2021-02-03 19:43:01 +0100

    [ADD] secondaryColor in constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color.fromRGBO(21, 27, 43, 1);
+const kSecondaryColor = Color.fromRGBO(71, 72, 178, 1);
 const kButtonColor = Color.fromRGBO(71, 72, 178, 1);
